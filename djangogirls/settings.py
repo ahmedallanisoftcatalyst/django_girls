298392bf81114d87fa3c17273db3d7577ae9697e
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2022-11-23 10:21:42 +0100

    update ignore

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "./static/"
 #uncomment the line below if to run on local machine.
 #STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
